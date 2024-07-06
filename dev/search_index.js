3f58c6c1f61e8e4fdda8310ba58ae4f4ac235bf9
Documenter.jl <documenter@juliadocs.github.io>
2024-07-06 09:10:26 +0000

    build based on 776ce0d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CentralSpinChain","category":"page"},{"location":"#CentralSpinChain","page":"Home","title":"CentralSpinChain","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CentralSpinChain.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CentralSpinChain]","category":"page"}]
+}
